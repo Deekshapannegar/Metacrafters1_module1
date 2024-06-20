21a2166dab4d5954bdd05fb713978387838c51f9
Deekshapannegar <deekshapannegar04@gmail.com>
2024-06-20 22:33:08 +0530

    Add files via upload

---
+++ errors.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.13; 
+
+contract Error_handling{ 
+    string public state = "Number is positive"; 
+    function _require(int256 _x) public view returns (string memory){ 
+        require(_x >= 0,"Number must be positive");
+        return state;
+        } 
+    
+    function _revert(int256 _x) public view returns (string memory){ 
+        if(_x <= 0){ 
+            revert("Number must be positive");
+            } 
+        return state;
+        } 
+        
+    function _assert(int256 _x) public view returns (string memory){ 
+        assert(_x  >= 0); 
+        return state;
+        }
+    }
